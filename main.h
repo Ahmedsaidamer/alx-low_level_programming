a34882bd31e42841c403967671e7a4ff4618bb94
Ahmedsaidamer <amer70470@gmail.com>
2023-07-05 10:50:56 -0700

    all recursion task

--- main.h
+++ main.h
@@ -1,17 +1,15 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar(char c);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
